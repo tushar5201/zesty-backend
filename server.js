b5d23e9ba2a1ecc7fc1c220c276ba7131a25d650
test <tusharlakadiya@gmail.com>
2025-02-17 21:24:34 +0530

    cors updated

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors(
 ));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://zesty-admin.vercel.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
