c5e6651fea56b0c7cfe13c460aad1b9d03983b14
test <tusharlakadiya@gmail.com>
2025-02-25 12:12:44 +0530

    cors error

--- server.js
+++ server.js
@@ -50,26 +50,25 @@ const allowedOrigins = [
     "https://zesty-restaurant-phi.vercel.app"
 ];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, origin);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: ["POST", "GET", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, origin);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     methods: ["POST", "GET", "DELETE", "PUT"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true
+// }));
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", req.headers.origin); // Dynamically set origin
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.options("*", (req, res) => {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.sendStatus(200);
+});
 
 
 // Database Connection
