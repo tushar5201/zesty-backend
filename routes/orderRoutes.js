e591b544b5f9aeea48f125c9f1af62397cfb8656
test <tusharlakadiya@gmail.com>
2025-04-03 09:34:50 +0530

    pagination

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -20,8 +20,12 @@ router.post("/add-order", async (req, res) => {
     }
 });
 
-router.get("/get-all-orders", async (req, res) => {
-    const orders = await Order.find();
+router.get("/get-all-orders/:page", async (req, res) => {
+    const { page } = req.params;
+    const orders = await Order.find()
+        .sort({ createdAt: "descending" })
+        .limit(10)
+        .skip(page * 10)
     return res.send(orders);
 })
 
